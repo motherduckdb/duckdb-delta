ae7af220e017a60d8174edb09f77b735574804db
Sam Ansmink <samansmink@hotmail.com>
2025-08-28 11:54:11 +0200

    fix: windows paths for copy_dir test macro

--- src/delta_macros.cpp
+++ src/delta_macros.cpp
@@ -44,9 +44,16 @@ JOIN
     tpcds_queries() as tpcds_queries on tpcds_queries."query"=query_string
 )";
 
+// Make proper function that supports remote FS
+#ifdef WIN32
+static constexpr auto FILE_COPY_MACRO = R"(
+    select write_blob(replace("dst_dir" || filename[length("src_dir")+1:], '/', '\'), content) from read_blob( "src_dir" || '/**')
+)";
+#else
 static constexpr auto FILE_COPY_MACRO = R"(
     select write_blob("dst_dir" || filename[length("src_dir")+1:], content) from read_blob( "src_dir" || '/**')
 )";
+#endif
 
 void DeltaMacros::RegisterTableMacro(ExtensionLoader &loader, const string &name, const string &query,
                                      const vector<string> &params, const child_list_t<Value> &named_params) {
